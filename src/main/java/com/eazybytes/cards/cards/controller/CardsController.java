e05aade709b4bad8e13c990ce7318da73c99661f
Rakesh Nair <crucifyrakesh@gmail.com>
2025-12-01 16:48:58 +0530

    Expose actuator endpoints to get updated api responses

--- src/main/java/com/eazybytes/cards/cards/controller/CardsController.java
+++ src/main/java/com/eazybytes/cards/cards/controller/CardsController.java
@@ -35,7 +35,7 @@ public class CardsController {
 
     private ICardsService iCardsService;
 
-    @Value("${build.info}")
+    @Value("${build.version}")
     private String buildInfo;
 
     @Autowired
