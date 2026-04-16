43197470618ce2cc67175c62be66f6cd70b5fb13
Rakesh Nair <crucifyrakesh@gmail.com>
2026-04-16 08:43:20 +0530

    opentelemetry and Tempo changes

--- src/main/java/com/eazybytes/cards/cards/controller/CardsController.java
+++ src/main/java/com/eazybytes/cards/cards/controller/CardsController.java
@@ -103,8 +103,10 @@ public class CardsController {
             @RequestParam
             @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
                                                      String mobileNumber) {
+        logger.debug("fetchCardDetails method start");
         CardsDto cardsDto = iCardsService.fetchCard(mobileNumber);
-        logger.debug("CorrelationId in fetchCardDetails : {}", correlationId);
+        //logger.debug("CorrelationId in fetchCardDetails : {}", correlationId);
+        logger.debug("fetchCardDetails method end");
         return ResponseEntity.status(HttpStatus.OK).body(cardsDto);
     }
 
